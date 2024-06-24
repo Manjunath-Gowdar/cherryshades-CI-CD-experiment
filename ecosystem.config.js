9fa2243f37d6732c9b9b65ea1e5ce6ef8866b638
Manjunath-Gowdar <manjunathaggowdar@gmail.com>
2024-06-25 01:52:05 +0530

    ecosystem.config.js file added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,11 @@
-module.exports = {
-  apps : [{
+import path from 'path';
+
+export default {
+  apps: [{
     name: "cherryshades-pm2",
     script: "./backend/server.js",
+    autorestart: true,
+   
     // More configuration options as needed
   }],
 };
