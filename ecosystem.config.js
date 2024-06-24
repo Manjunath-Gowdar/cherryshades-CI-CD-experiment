ac16f4f295e4a41d709922e2d1a92b760ff7ac33
Manjunath-Gowdar <manjunathaggowdar@gmail.com>
2024-06-25 01:26:50 +0530

    ecosystem.config.js file added

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps : [{
+    name: "cherryshade-pm2",
+    script: "./backend/server.js",
+    env: {
+      NODE_ENV: "production",
+      PORT: 5000,
+    },
+    // More configuration options as needed
+  }],
+};
