5397c15c21cc917201288d9777a7600ce3ccafa6
Manjunath-Gowdar <manjunathaggowdar@gmail.com>
2024-06-28 01:34:08 +0530

    updated deploy.yml

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -36,7 +36,7 @@ const HomeScreen = ({ match }) => {
         </Link>
       )}
       <h1 style={{ paddingTop: '20px', marginBottom: '0px' }}>
-        Latest Products, testing CI/CD again
+        Latest Products, testing CI/CD again & again
       </h1>
       {loading ? (
         <Loader />
